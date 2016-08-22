6ce1756c92cd5ed3da07970df08161221de00972
William Oliveira de Lagos <william.lagos1@gmail.com>
2016-08-22 18:08:54 -0300

    Added tests structure

---
+++ kombi/tests.py
@@ -0,0 +1,11 @@
+""" Main app test module """
+from django.test import TestCase
+# from kombi.models import Delivery
+
+class DeliveryTestCase(TestCase):
+    """ Delivery test cases """
+    def setUp(self):
+        pass
+    def test_deliveries(self):
+        """ Tests delivery model creation """
+        pass
