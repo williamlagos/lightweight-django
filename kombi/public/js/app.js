fd693e6a651db4f221bbe16a48baf58ab86b1957
William Oliveira de Lagos <william.lagos1@gmail.com>
2016-09-29 17:37:39 -0300

    Fixed mistyping in error handler for websocket

--- kombi/public/js/app.js
+++ kombi/public/js/app.js
@@ -9,7 +9,7 @@ var app = (function($) {
         var navbar = new app.views.NavbarView();
         ws.onopen = function(event){ console.log('socket open'); };
         ws.onclose = function(event){ console.log('socket closed'); };
-        ws.onerror = function(error){ console.log('error:', err); };
+        ws.onerror = function(error){ console.log('error:', error); };
         ws.onmessage = function(event){ console.log('message:', event.data); };
     });
     return app;
